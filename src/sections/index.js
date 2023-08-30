03c2e1ce7cfc086796b547d3409631f0c7f03beb
mohamed <mohamedgelany2030@gmail.com>
2023-08-30 14:44:40 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import specialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    SuperQuality,
+    Footer,
+    specialOffers,
+    CustomerReviews,
+    Subscribe
+}
